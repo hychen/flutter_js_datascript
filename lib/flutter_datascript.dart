1997325d71624b5eb50e35cc4ac07813bd9111aa
Hsin Yi, Chen <ossug.hychen@gmail.com>
2021-11-08 17:17:09 +0800

    Init.

---
+++ lib/flutter_datascript.dart
@@ -0,0 +1,232 @@
+library flutter_datascript;
+
+import 'dart:io';
+import 'package:flutter_js/flutter_js.dart';
+
+typedef DB = Object;
+typedef Schema = Map;
+typedef PullResult = Map;
+
+/// Transaction Report
+class TxReport {
+  /// db value before transaction
+  final DB dbBefore;
+
+  /// db value after transaction
+  final DB dbAfter;
+
+  /// plain datoms that were added/retracted from db-before
+  final List txData;
+
+  /// map of tempid from tx-data => assigned entid in db-after
+  final dynamic tempids;
+
+  /// the exact value you passed as `tx-meta`
+  final dynamic txMeta;
+
+  TxReport(
+      {required this.dbBefore,
+      required this.dbAfter,
+      required this.tempids,
+      required this.txData,
+      this.txMeta});
+}
+
+/// This class implements Datascript Public Javascript APIs.
+class FlutterDatascript {
+  final jsEngine = getJavascriptRuntime(xhr: false);
+
+  FlutterDatascript() {
+    _tryLoadJsBundle();
+  }
+
+  Future<void> _tryLoadJsBundle() async {
+    String bundleLoaded = jsEngine.evaluate("""
+    (typeof vendor === 'undefined') ? 0 : 1;
+    """).stringResult;
+    if (bundleLoaded == '0') {
+      await _loadJsBundle();
+    }
+  }
+
+  Future<void> _loadJsBundle() async {
+    jsEngine.evaluate("""
+    var window = global = globalThis;
+    """);
+    String jsBundle = await File('./assets/js/dist/bundle.js').readAsString();
+    jsEngine.evaluate(jsBundle);
+  }
+
+  // -------------------------------------------------------------------------
+  // JS public APIs.
+  // -------------------------------------------------------------------------
+
+  /// Creates an empty database with an optional schema.
+  Future<DB> emptyDb({Schema? shcema}) {
+    throw UnimplementedError();
+  }
+
+  /// Low-level fn for creating database quickly from a trusted sequence of
+  /// datoms.
+  Future<DB> initDb({Schema? schema}) {
+    throw UnimplementedError();
+  }
+
+  /// Converts db into a data structure (not string!) that can be fed to serializer
+  /// of your choice (e.g. `js/JSON.stringify` in CLJS, `cheshire.core/generate-string`
+  /// or `jsonista.core/write-value-as-string` in CLJ).
+  ///
+  /// On JVM, `serializable` holds a global lock that prevents any two serializations
+  /// to run in parallel (an implementation constraint, be aware).
+  Future serializable(db, Object? opts) {
+    throw UnimplementedError();
+  }
+
+  /// Creates db from a data structure (not string!) produced by serializable.
+  Future fromSerializable(serializable, opts) {
+    throw UnimplementedError();
+  }
+
+  /// Executes a datalog query. See [docs.datomic.com/on-prem/query.html](https://docs.datomic.com/on-prem/query.html)
+  Future q(String query, List inputs) {
+    throw UnimplementedError();
+  }
+
+  /// Fetches data from database using recursive declarative description.
+  /// See [docs.datomic.com/on-prem/pull.html](https://docs.datomic.com/on-prem/pull.html).
+  Future<List<PullResult>> pull(DB db, pattern, int eid) {
+    throw UnimplementedError();
+  }
+
+  /// Same as [[pull]], but accepts sequence of ids and returns sequence of maps.
+  Future<List<PullResult>> pullMany(DB db, pattern, List<int> eids) {
+    throw UnimplementedError();
+  }
+
+  /// Applies transaction to an immutable db value, returning new immutable db
+  /// value. Same as `(:db-after (with db tx-data))`.
+  Future dbWith(DB db, List entities) {
+    throw UnimplementedError();
+  }
+
+  /// Retrieves an entity by its id from database. Entities are lazy map-like
+  /// structures to navigate DataScript database content.
+  Future entity(DB db, int eid) {
+    throw UnimplementedError();
+  }
+
+  /// Forces all entity attributes to be eagerly fetched and cached. Only usable for debug output.
+  Future touch(Object e) {
+    throw UnimplementedError();
+  }
+
+  /// Returns a db that entity was created from.
+  Future entityDb(Object e) {
+    throw UnimplementedError();
+  }
+
+  /// Returns a view over database that has same interface but only includes
+  /// datoms for which the `(pred db datom)` is true. Can be applied multiple
+  /// times.
+  Future filter(db, pred) {
+    throw UnimplementedError();
+  }
+
+  /// Returns `true` if this database was filtered using [[filter]], `false`
+  /// otherwise."
+  Future<bool> isFiltered(DB db) {
+    throw UnimplementedError();
+  }
+
+  /// Creates a mutable reference (a “connection”) to an empty immutable
+  /// database.
+  Future createConn({Schema? schema}) {
+    throw UnimplementedError();
+  }
+
+  /// Creates a mutable reference to a given immutable database.
+  /// See [[createConn]].
+  Future connFromDb(DB db) {
+    throw UnimplementedError();
+  }
+
+  /// Creates an empty DB and a mutable reference to it. See [createConn].
+  Future connFromDatoms(dataoms, {Schema? schema}) {
+    throw UnimplementedError();
+  }
+
+  /// Returns the underlying immutable database value from a connection.
+  Future<DB> db(conn) {
+    throw UnimplementedError();
+  }
+
+  /// Applies transaction the underlying database value and atomically updates
+  /// connection reference to point to the result of that transaction, new db
+  /// value.
+  ///
+  /// Returns [TxReport].
+  Future<TxReport> transact(conn, List txDeta, {txMeta}) {
+    throw UnimplementedError();
+  }
+
+  /// Forces underlying `conn` value to become `db`.
+  /// Will generate a [TxReport] that will remove everything from old value
+  /// and insert everything from the new one."
+  Future resetConn(conn, db, {txMeta}) {
+    throw UnimplementedError();
+  }
+
+  /// Listen for changes on the given connection.
+  /// Returns a key as string.
+  String listen(conn, Function callback, {String? key}) {
+    throw UnimplementedError();
+  }
+
+  /// Removes registered listener from connection. See also [[listen!]].
+  unListen(conn, String key) {
+    throw UnimplementedError();
+  }
+
+  /// Does a lookup in tempids map, returning an entity id that tempid was
+  /// resolved to.
+  resolveTempid(Map tempids, int tempid) {
+    throw UnimplementedError();
+  }
+
+  /// Index lookup. Returns a sequence of datoms (lazy iterator over actual DB
+  /// index) which components (e, a, v) match passed arguments.
+  Future<Function> datoms(
+      db, index, Object? c1, Object? c2, Object? c3, Object? c4) {
+    throw UnimplementedError();
+  }
+
+  /// Similar to [[datoms]], but will return datoms starting from specified
+  /// components and including rest of the database until the end of the index.
+  Future seekDatoms(db, index, Object? c1, Object? c2, Object? c3, Object? c4) {
+    throw UnimplementedError();
+  }
+
+  /// Returns part of `:avet` index between `[_ attr start]` and `[_ attr end]`
+  /// in AVET sort order.
+  ///
+  /// Same properties as [datoms].
+  Future indexRange(DB db, attr, start, end) {
+    throw UnimplementedError();
+  }
+
+  /// Generates a UUID that grow with time. Such UUIDs will always go to the
+  /// end of the index and that will minimize insertions in the middle.
+  ///
+  /// @TODO: implemented by n Dart.
+  Future<String> squuid({int? msec}) {
+    throw UnimplementedError();
+  }
+
+  /// Returns time that was used in [[squuid]] call, in milliseconds, rounded
+  /// to the closest second.
+  ///
+  /// @TODO: implemented by Dart.
+  Future<int> squuidTimeMillis(uuid) {
+    throw UnimplementedError();
+  }
+}
---
+++ test/flutter_datascript_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_datascript/flutter_datascript.dart';
+
+void main() {
+  test('js-like example works', () async {
+    var d = FlutterDatascript();
+
+    // create DB schema, a regular JS Object
+    var schema = {
+      "aka": {":db/cardinality": ":db.cardinality/many"},
+      "friend": {":db/valueType": ":db.type/ref"}
+    };
+
+    // Use JS API to create connection and add data to DB
+    // create connection using schema
+    var conn = await d.createConn(schema: schema);
+
+    // setup listener called main
+    // pushes each entity (report) to an Array of reports
+    // This is just a simple example. Make your own!
+    var reports = [];
+    d.listen(conn, (report) => {reports.add(report)}, key: 'main');
+
+    // define initial datoms to be used in transaction
+    var datoms = [
+      {
+        ":db/id": -1,
+        "name": "Ivan",
+        "age": 18,
+        "aka": ["X", "Y"]
+      },
+      {
+        ":db/id": -2,
+        "name": "Igor",
+        "aka": ["Grigory", "Egor"]
+      },
+      // use :db/add to link datom -2 as friend of datom -1
+      [":db/add", -1, "friend", -2]
+    ];
+
+    // Tx is Js Array of Object or Array
+    // pass datoms as transaction data
+    await d.transact(conn, datoms, txMeta: "initial info about Igor and Ivan");
+
+    var db = await d.db(conn);
+    // Fetch names of people who are friends with someone 18 years old
+    // query values from conn with JS API
+    var result = await d.q(
+        '[:find ?n :in \$ ?a :where [?e "friend" ?f] [?e "age" ?a] [?f "name" ?n]]',
+        [db, 18]);
+
+    // print query result to console!
+    print(result); // [["Igor"]]
+  });
+}
